2248587f5b80a1604c84769eacab02ce09a625e9
Altynay Zholeu <81471149+zholeu@users.noreply.github.com>
2024-12-10 13:59:41 +0300

    Update OrderListController.java

--- controller/OrderListController.java
+++ controller/OrderListController.java
@@ -1,11 +1,7 @@
 package com.springeasystock.easystock.controller;
 
-import com.springeasystock.easystock.dto.CustomerDTO;
-import com.springeasystock.easystock.dto.EmployeeDTO;
-import com.springeasystock.easystock.dto.ItemDTO;
-import com.springeasystock.easystock.dto.OrderListDTO;
+import com.springeasystock.easystock.record.OrderListDTO;
 import com.springeasystock.easystock.model.OrderList;
-import com.springeasystock.easystock.service.EmployeeService;
 import com.springeasystock.easystock.service.OrderListService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -36,21 +32,21 @@ public class OrderListController {
 
     @PutMapping("/{orderListId}/item/{itemId}")
     public OrderList assignItemToOrderList(
-            @PathVariable Integer orderListId,
-            @PathVariable Integer itemId
+            @PathVariable Long orderListId,
+            @PathVariable Long itemId
     ){
         return orderListService.assignItemToOrderList(orderListId, itemId);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<OrderListDTO> getEmployeeById(@PathVariable("id") Integer orderListId){
+    public ResponseEntity<OrderListDTO> getEmployeeById(@PathVariable("id") Long orderListId){
         OrderListDTO orderListDTO = orderListService.getOrderListById(orderListId);
         return ResponseEntity.ok(orderListDTO);
 
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<OrderListDTO> updateEmployee(@PathVariable("id")Integer orderListId,
+    public ResponseEntity<OrderListDTO> updateEmployee(@PathVariable("id")Long orderListId,
                                                   @RequestBody OrderListDTO updatedItem){
         OrderListDTO orderListDTO = orderListService.updateOrderList(orderListId, updatedItem);
         return ResponseEntity.ok(orderListDTO);
@@ -58,10 +54,19 @@ public class OrderListController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable("id")Integer orderListId){
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id")Long orderListId){
         orderListService.deleteOrderList(orderListId);
         return ResponseEntity.ok("Deleted Successfully!");
 
+    }
+
+    @DeleteMapping("/{orderListId}/item/{itemId}")
+    public ResponseEntity<String> unassignItemFromOrderList(
+            @PathVariable Long orderListId,
+            @PathVariable Long itemId
+    ) {
+        orderListService.unassignItemFromOrderList(orderListId, itemId);
+        return ResponseEntity.ok("Item with ID " + itemId + " was successfully unassigned from OrderList with ID " + orderListId + "!");
     }
 
 
