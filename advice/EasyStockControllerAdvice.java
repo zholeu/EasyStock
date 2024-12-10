4561dacd8c289ae44ce1a33b27d4054c725f536c
Altynay Zholeu <81471149+zholeu@users.noreply.github.com>
2024-12-10 13:27:37 +0300

    Update EasyStockControllerAdvice.java

--- advice/EasyStockControllerAdvice.java
+++ advice/EasyStockControllerAdvice.java
@@ -1,21 +1,18 @@
 package com.springeasystock.easystock.advice;
 
-import com.springeasystock.easystock.Exception.CustomNotFoundException;
+import com.springeasystock.easystock.exception.CustomNotFoundException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-
-import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class EasyStockControllerAdvice {
@@ -51,7 +48,6 @@ public class EasyStockControllerAdvice {
         errorResponse.put("message", "Constraint violation");
         errorResponse.put("details", ex.getMessage());
         errorResponse.put("status", HttpStatus.BAD_REQUEST.value()); //400
-
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -62,8 +58,21 @@ public class EasyStockControllerAdvice {
         errorResponse.put("message", "Data integrity violation");
         errorResponse.put("details", extractFieldFromException(ex));
         errorResponse.put("status", HttpStatus.CONFLICT.value()); // 409
-
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("message", "Validation failed");
+        errorResponse.put("details", fieldErrors);
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value()); // 400
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
